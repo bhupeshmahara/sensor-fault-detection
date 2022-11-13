88a32033b91a7efe588fdaf3ae0e5e49e7a1420f
Bhupesh Mahara <bhupeshmahara@gmail.com>
2022-11-13 13:07:38 +0530

    updated files

--- sensor/data_access/sensor_data.py
+++ sensor/data_access/sensor_data.py
@@ -3,7 +3,7 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 import json
-from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.configuration.mongodb_connections import MongoDBClient
 from sensor.constant.database import DATABASE_NAME
 from sensor.exception import SensorException
 
